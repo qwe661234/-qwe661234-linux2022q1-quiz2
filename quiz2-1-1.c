e40e16b09fdd2419d3f01ca07521369447a8d334
qwe661234 <qwe661234@gmail.com>
2022-03-04 16:59:20 +0800

    Update quiz1 Answer file

---
+++ quiz2-1-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t average(uint32_t a, uint32_t b)
+{
+    return (a >> 1) + (b >> 1) + (a & b & 1);
+}
+
+int main(int argc, char** argv) {
+    uint32_t a = 3, b = 5;
+    printf("%u", average(a, b));
+    return 0;
+}
